2b005c31c65ae6b6b133e9d4ee9e0a029cd4cc13
sofi4ka <85256551+kotova-s0@users.noreply.github.com>
2022-04-04 21:54:42 +0300

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    ifstream fin("num2020.txt");
+    if (fin.is_open()) {
+        string n;
+        vector <int> num;
+        while (getline(fin, n)) {
+            if (atoi(n.c_str())) {
+                num.push_back(atoi(n.c_str()));
+            }
+        }
+        for (int i = 0; i < num.size() - 1; i++) {
+            for (int j = i + 1; j < num.size(); j++) {
+                if (num[i] + num[j] == 2020) cout << num[i] * num[j] << endl;
+            }
+        }
+    }
+    else cout << "can't open file";
+    fin.close();
+}
